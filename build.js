ea11428a85ad6516b992efc545e1149c6041c9e7
TaylorMade <warriorotk18@gmail.com>
2022-03-24 00:19:14 -0600

    add while loop

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
-function addThis(big, small) {
-    const up = big + small;
-    console.log(up);
-}
+// function addThis(big, small) {
+//     const up = big + small;
+//     console.log(up);
+// }
 
-addThis(143, 45);
+// addThis(143, 45);
+
+let counter = 0;
+while (counter < 10) {
+    console.log(counter);
+    counter += 1;
+}
